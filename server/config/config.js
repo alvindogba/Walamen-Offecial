205a822ff8b720103a8204754027af3dc4fb0b22
alvindogba <alvindr2000@gmail.com>
2026-01-14 19:36:47 +0000

    fixed the env configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,31 +1,15 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
-// Determine the current environment if NODE_ENV is not avaliable, (default to 'development')
 const env = process.env.NODE_ENV || "development";
+
 const config = {
   development: {
-    username: process.env.DB_USERNAME, 
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD ,
-    database: process.env.PROD_DB_NAME ,
-    host: process.env.PROD_DB_HOST ,
-    port: process.env.PROD_DB_PORT,
+    port: Number(process.env.DB_PORT) || 5432,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -33,7 +17,21 @@ const config = {
         rejectUnauthorized: false,
       },
     },
-     
+  },
+
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    port: Number(process.env.PROD_DB_PORT) || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
 
